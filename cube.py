50b631bcf868c3e8861a821d2253374283aedaec
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2025-10-06 11:38:17 -0300

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,8 +1,9 @@
 from hit import HitBox, HitBoxOBB
 import numpy as np
 import glm
+from model import Model
 
-class Cube:
+class Cube(Model):
     def __init__(self, position=(0,0,0),rotation=(0,0,0), scale=(1,1,1),name="Cube"):
         self.name=name
         self.position = glm.vec3(*position)
@@ -10,8 +11,7 @@ class Cube:
         self.scale = glm.vec3(*scale)
         self._colision= HitBoxOBB(get_model_matrix = lambda: self.get_model_matrix())
 
-        # Array para crear el VBO: Vertex Buffer Object: almacena los vértices y colores de un cubo
-        self.vertices = np.array([
+        vertices = np.array([
             # posiciones        # colores
             -1, -1, -1,        1, 0, 0,
              1, -1, -1,        0, 1, 0,
@@ -23,8 +23,22 @@ class Cube:
             -1,  1,  1,        0, 0, 0
         ], dtype='f4')
 
-        # Array para crear el IBO: Index Buffer Object: define las caras del cubo
-        self.indices = np.array([
+        colors = np.array([
+            1,0,0, 0,1,0, 0,0,1, 1,1,0,
+            1,0,1, 0,1,1, 1,1,1, 0,0,0
+        ], dtype='f4')
+
+        normals = np.array([
+            -1,-1,-1, 1,-1,-1, 1,1,-1, -1,1,-1,
+            -1,-1,1, 1,-1,1, 1,1,1, -1,1,1
+        ], dtype='f4')
+
+        textcoords = np.array([
+            0,0, 1,0, 1,1, 0,1,
+            0,0, 1,0, 1,1, 0,1
+        ], dtype='f4')
+
+        indices = np.array([
             0, 1, 2, 2, 3, 0,  # atrás
             4, 5, 6, 6, 7, 4,  # frente
             0, 4, 7, 7, 3, 0,  # izquierda
@@ -32,15 +46,16 @@ class Cube:
             3, 2, 6, 6, 7, 3,  # arriba
             0, 1, 5, 5, 4, 0   # abajo
         ], dtype='i4')
+        super().__init__(vertices, indices, colors, normals, textcoords)
 
     def check_hit(self,origin, direction):
         return self._colision.check_hit(origin, direction)
 
     def get_model_matrix(self):
-        model=glm.mat4(1)
-        model=glm.translate(model,self.position)
-        model=glm.rotate(model,glm.radians(self.rotation.x),glm.vec3(1,0,0))
-        model=glm.rotate(model,glm.radians(self.rotation.y),glm.vec3(0,1,0))
-        model=glm.rotate(model,glm.radians(self.rotation.z),glm.vec3(0,0,1))
-        model=glm.scale(model,self.scale)
+        model = glm.mat4(1)
+        model = glm.translate(model, self.position)
+        model = glm.rotate(model, glm.radians(self.rotation.x %360), glm.vec3(1, 0, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.y %360), glm.vec3(0, 1, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.z %360), glm.vec3(0, 0, 1))
+        model = glm.scale(model,self.scale)
         return model
